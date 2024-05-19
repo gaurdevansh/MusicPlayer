73a9a2c4181a058550ac006ceda206943fbe5d17
Devansh <devanshgaur75@gmail.com>
2024-05-19 21:52:25 +0530

    Fix the bottom navigation issue

--- app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.musicplayer.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import android.widget.FrameLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.musicplayer.R
 import com.example.musicplayer.databinding.ActivityMainBinding
@@ -14,19 +20,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bottomNavView: BottomNavigationView
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
 
-        bottomNavView = binding.bottomNavigation
+    override fun onStart() {
+        super.onStart()
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        //navController = navHostFragment.navController
         navController = navHostFragment.navController
-        bottomNavView.setupWithNavController(navController)
-
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
