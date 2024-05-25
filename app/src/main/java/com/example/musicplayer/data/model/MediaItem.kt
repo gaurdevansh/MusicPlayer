3e5718e513a966f34a9c97c31db6211ceec04905
Devansh <devanshgaur75@gmail.com>
2024-05-25 19:37:10 +0530

    Read media items from storage

---
+++ app/src/main/java/com/example/musicplayer/data/model/MediaItem.kt
@@ -0,0 +1,8 @@
+package com.example.musicplayer.data.model
+
+data class MediaItem(
+    val id: Long,
+    val title: String,
+    val artist: String,
+    val path: String
+)
--- app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.example.musicplayer.ui
 
-import android.content.Context
+import android.content.ContentResolver
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.view.View
-import android.widget.FrameLayout
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.musicplayer.R
+import com.example.musicplayer.data.model.MediaItem
 import com.example.musicplayer.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -26,12 +25,74 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    override fun onStart() {
-        super.onStart()
+        
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
+        
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_CODE_READ_EXTERNAL_STORAGE
+            )
+        } else {
+            loadMediaItems()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_READ_EXTERNAL_STORAGE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadMediaItems()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    
+    private fun loadMediaItems() {
+        val mediaItemList = mutableListOf<MediaItem>()
+        val contentResolver: ContentResolver = contentResolver
+        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DATA,
+        )
+        val selection = "${MediaStore.Audio.Media.IS_MUSIC} != 0"
+        val sortOrder = "${MediaStore.Audio.Media.TITLE} ASC"
+
+        contentResolver.query(uri, projection, selection, null, sortOrder).use { cursor ->
+            val idColumn = cursor?.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleColumn = cursor?.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistColumn = cursor?.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val dataColumn = cursor?.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)
+
+            if (cursor != null) {
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idColumn!!)
+                val title = cursor.getString(titleColumn!!)
+                val artist = cursor.getString(artistColumn!!)
+                val path = cursor.getString(dataColumn!!)
+
+                mediaItemList.add(MediaItem(id, title, artist, path))
+                }
+            }
+        }
+
+        for (mediaItem in mediaItemList) {
+            println("Media Item: ${mediaItem.title} by ${mediaItem.artist}")
+        }
     }
 }
